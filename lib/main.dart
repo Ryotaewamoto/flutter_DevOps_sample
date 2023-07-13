973cd489a917dba617fa0d168b2b3d009719cf58
ryotaewamoto <lengtaiyanben@gmail.com>
2023-07-13 21:41:34 +0900

    feat: README を編集
    feat: 新機能
    fix: バグ修正・レビュー反映
    docs: ドキュメントの変更
    style: コードのロジックに影響を与えない変更（ホワイトスペース、フォーマット、セミコロンの欠落など）
    refactor: バグ修正でも機能追加でもないコード変更
    perf: パフォーマンス向上
    test: テストの追加や既存のテストの修正
    chore: ビルドプロセス、またはドキュメント生成などの補助ツールやライブラリの変更

--- lib/main.dart
+++ lib/main.dart
@@ -6,29 +6,75 @@ void main() {
   runApp(
     DevicePreview(
       enabled: kIsWeb,
-      builder: (context) => const MainApp(), // Wrap your app
+      builder: (context) => const MyApp(), // Wrap your app
     ),
   );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter DevOps Sample'),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.light,
+        colorSchemeSeed: Colors.green[700],
+      ),
+      darkTheme: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.dark,
+        colorSchemeSeed: Colors.green[700],
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
         ),
-        body: const Center(
-          child: Column(
-            children: [
-              Text('Hello World!!!'),
-              Text('You have a good day!'),
-            ],
-          ),
-        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
       ),
     );
   }
